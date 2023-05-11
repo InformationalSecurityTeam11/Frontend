8f62a8fce1cf3efe529881c2c7e0b5d094406434
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-05-11 20:11:54 +0200

    added navbar component

--- InformationSecurityFrontend/src/app/app.module.ts
+++ InformationSecurityFrontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {RegistrationComponent} from "./modules/auth/registration/registration.component";
 import {LoginComponent} from "./modules/auth/login/login.component";
+import { NavbarComponent } from './modules/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegistrationComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
