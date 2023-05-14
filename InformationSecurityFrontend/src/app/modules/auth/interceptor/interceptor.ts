ba00860e0286a591d245bdfe1c050b7d939af98b
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-05-14 14:18:12 +0200

    removed unused lines

--- InformationSecurityFrontend/src/app/modules/auth/interceptor/interceptor.ts
+++ InformationSecurityFrontend/src/app/modules/auth/interceptor/interceptor.ts
@@ -5,11 +5,9 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class Interceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Add authorization token to the headers
     let token = localStorage.getItem('user');
     if (token) {
       token = token.replace(/"/g, '');
-      console.log("tokencic: ", token)
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
